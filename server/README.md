73192e774fc5c8420aaf6d961d21cf64d1859e1b
wayne <wodutoit@gmail.com>
2020-03-05 13:35:23 +0200

    added sample

---
+++ server/README.md
@@ -0,0 +1,17 @@
+## Installation
+
+```bash
+$ npm install
+```
+
+## Running the app
+
+```bash
+# development
+$ npm run start
+```
+
+## Run Client
+
+```open the index.html file from the client folder in your web browser
+```
---
+++ server/src/chat/chat.gateway.ts
@@ -0,0 +1,36 @@
+import { WebSocketGateway, WebSocketServer, SubscribeMessage, OnGatewayConnection, OnGatewayDisconnect, MessageBody, WsResponse } from '@nestjs/websockets';
+import { from, Observable } from 'rxjs';
+
+@WebSocketGateway()
+export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
+    @WebSocketServer() server;
+    users: number = 0;
+    
+    async handleConnection(){
+
+        // A client has connected
+        this.users++;
+
+        // Notify connected clients of current users
+        this.server.emit('users', this.users);
+        console.log('connection');
+
+    }
+
+    async handleDisconnect(){
+
+        // A client has disconnected
+        this.users--;
+
+        // Notify connected clients of current users
+        this.server.emit('users', this.users);
+
+    }
+
+    @SubscribeMessage('chat')
+    onChat(@MessageBody() data: string): string {
+        console.log(`server received chat ${data}`);
+        this.server.emit('chat', data);
+        return data;
+    }
+}
